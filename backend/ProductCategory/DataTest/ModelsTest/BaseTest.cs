64d4ec577105162349522eadb8d731bc88c32595
Matheus Frutuoso <matheusfx5@gmail.com>
2021-09-01 08:40:14 -0300

    Base model created and tested

---
+++ backend/ProductCategory/DataTest/ModelsTest/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.ModelsTest
+{
+    public class BaseTest
+    {
+        [Fact]
+        public void BaseInstanceTest()
+        {
+            //Arrange
+            //Act
+            Action a = () => Activator.CreateInstance<Base>();
+            //Assert
+            Assert.Throws<MissingMethodException>(a);
+        }
+    }
+}
